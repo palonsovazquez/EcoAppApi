cae2931750df94703e5c5a93e85be001cfd067eb
palonsovazquez <pav.vigo@gmail.com>
2020-05-18 16:20:52 +0200

    reparado get

---
+++ EcoApp/EcoApp/cor/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from EcoApp.cor.serializers import UserSerializer, GroupSerializer ,ProductSerializer
+from EcoApp.cor.models import Productos
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Product to be viewed or edited.
+    """
+    queryset = Productos.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
