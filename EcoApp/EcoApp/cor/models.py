fe33faa7a69b56f817ce80fa9a0becf31fdeb676
palonsovazquez <pav.vigo@gmail.com>
2020-05-27 21:53:25 +0200

    cambiado usuario productos

--- EcoApp/EcoApp/cor/models.py
+++ EcoApp/EcoApp/cor/models.py
@@ -3,7 +3,8 @@ import uuid
 from django.db import models
 from django.db import models
 from django.utils import timezone
-from drf_firebase_auth.models import FirebaseUserProvider, FirebaseUser
+from drf_firebase_auth.models import  FirebaseUser
+
 
 
 #model of Component
@@ -50,7 +51,7 @@ class Productos(models.Model):
     components = models.ManyToManyField(Component)
     image = models.ImageField(upload_to="Products/Images", height_field=None, width_field=None, max_length=100,)
 #    uid = models.id = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=True)
-    fbuser = models.ForeignKey(FirebaseUser,on_delete=models.DO_NOTHING , null=True ,default='1')
+    fbuser = models.ForeignKey('auth.User',on_delete=models.DO_NOTHING , null=True ,default='1',editable=False)
     #nick = models.CharField(max_length=200)
     #avatar = models.ImageField(upload_to="Users/Avatars", height_field=None, width_field=None, max_length=100,)
     description = models.TextField(null=True, default="")
